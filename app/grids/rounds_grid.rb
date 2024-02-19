34102950b0f1abbc59411567b296a8c3229b505e
Kevin Luo <kevin.luo@telus.com>
2024-02-19 10:10:24 -0500

    adapt bulma to kaminari's pagination

--- app/grids/rounds_grid.rb
+++ app/grids/rounds_grid.rb
@@ -7,9 +7,13 @@ class RoundsGrid < BaseGrid
   filter(:name)
   filter(:draw_at, :date, range: true)
 
-  column(:number)
-  date_column(:draw_at)
-  column(:name)
-  column(:size)
-  column(:crs)
+  column(:round_number, header: '#', order: false) do
+    number
+  end
+  column(:draw_at, header: 'Date') do
+    draw_at.strftime("%B %d, %Y")
+  end
+  column(:name, header: 'Round Type')
+  column(:size, header: 'Invitations Issued')
+  column(:crs, header: 'Lowest CRS')
 end
