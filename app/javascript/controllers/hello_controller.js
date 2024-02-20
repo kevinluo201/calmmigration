e5ba315c670ddb4da744ab881ddcf160b17038ce
Kevin Luo <kevin.luo@telus.com>
2024-02-19 22:55:16 -0500

    add unsubscribe and update elements according to subscribedValue

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,23 +2,72 @@ import { Controller } from "@hotwired/stimulus"
 import { post } from '@rails/request.js'
 
 export default class extends Controller {
+  static targets = ["text", "button"];
+  static values = {
+    vapidPublicKey: String,
+    subscribed: Boolean
+  }
+
+  connect() {
+    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+      serviceWorkerRegistration.pushManager.getSubscription().then((subscription) => {
+        if (subscription) {
+          this.subscribedValue = true;
+        } else {
+          this.subscribedValue = false;
+        }
+        this.updateElements();
+      });
+    });
+  }
+
+  updateElements() {
+    if (this.subscribedValue) {
+      this.textTarget.textContent = 'You already subscribed to push notifications!';
+      this.buttonTarget.textContent = 'Unsubscribe';
+    } else {
+      this.textTarget.textContent = 'Subscribe to push notifications!';
+      this.buttonTarget.textContent = 'Subscribe';
+    }
+  }
+
+  toggleSubscription() {
+    if (this.subscribedValue) {
+      this.unsubscribe();
+    } else {
+      this.subscribe();
+    }
+  }
+
   async subscribe() {
     navigator.serviceWorker.register('/service-worker.js')
-    // When serviceWorker is supported, installed, and activated,
-    // subscribe the pushManager property with the vapidPublicKey
-    const options = {
-      userVisibleOnly: true,
-      applicationServerKey: window.vapidPublicKey
-    };
+
     navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-      serviceWorkerRegistration.pushManager.subscribe(options).then(
+      serviceWorkerRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: window.vapidPublicKey
+      }).then(
         async (pushSubscription) => {
           const response = await post('/webpush_notifications', { body: pushSubscription.toJSON() })
           if (response.ok) {
-            console.log('Push subscription created successfully');
+            this.subscribedValue = true;
+            this.updateElements();
           }
         }
       );
+    });
+  }
+
+  async unsubscribe() {
+    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+      serviceWorkerRegistration.pushManager.getSubscription().then((subscription) => {
+        if (subscription) {
+          subscription.unsubscribe().then(() => {
+            this.subscribedValue = false;
+            this.updateElements();
+          });
+        }
+      });
     });
   }
 }
