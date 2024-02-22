93c764c55144d96e9a523ca22df44f71101af906
Kevin Luo <kevin.luo@telus.com>
2024-02-21 23:05:38 -0500

    send notification after save

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,6 +2,10 @@ class Round < ApplicationRecord
   ROUND_BASE_URL = 'https://www.canada.ca/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds/invitations.html'
   GET_ROUNDS_JSON_URL = 'https://www.canada.ca/content/dam/ircc/documents/json/ee_rounds_4_en.json'
 
+  attr_accessor :send_notifications_after_save
+
+  after_commit :send_notifications, if: -> { send_notifications_after_save }
+
   def round_url
     ROUND_BASE_URL + "?q=#{number}"
   end
@@ -15,5 +19,11 @@ class Round < ApplicationRecord
       },
       tag: "express-entry-round-#{number}"
     }
+  end
+
+  private
+
+  def send_notifications
+    WebpushNotification.send_notifications(notification_message)
   end
 end
