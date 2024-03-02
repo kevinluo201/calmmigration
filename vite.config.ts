c5b35c73a474d1aaf05ec76f40d75287ca17b39d
Kevin Luo <kevin.luo@telus.com>
2024-03-02 11:20:13 -0500

    remvoe useless config

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import RubyPlugin from 'vite-plugin-ruby'
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-  ],
-  esbuild: {
-    loader: "tsx",
-    include: [
-      "app/**/*.jsx",
-      "app/**/*.tsx",
-      "node_modules/**/*.jsx",
-      "node_modules/**/*.tsx",
-    ],
-  }
+  ]
 })
