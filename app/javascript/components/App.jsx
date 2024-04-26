a43213625a7ba78c337645487f01ed33b381d1dd
tracy <200548417@student.georgianc.on.ca>
2024-04-26 19:09:38 -0400

    adjust Trends page

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const App = () => {
   const [rounds, setRounds] = useState([]);
-  const [selectedYear, setSelectedYear] = useState(null);
-  const [selectedName, setSelectedName] = useState(null);
+  const [selectedYear, setSelectedYear] = useState(2024);
+  const [selectedName, setSelectedName] = useState("");
 
   const fetchRounds = async () => {
     try {
@@ -13,14 +13,14 @@ const App = () => {
         throw new Error('Network response was not ok.');
       }
       const data = await response.json();
-      console.log(data);
+      
       setRounds(data);
     } catch (error) {
       console.log('An error occurred while fetching the data.');
     }
   }
 
-  // Filter data based on selected year and name
+  
   const filterDataByYearAndName = (year, name) => {
     setSelectedYear(year);
     setSelectedName(name);
@@ -28,13 +28,12 @@ const App = () => {
 
   // Filtered data based on selected year and name
   //const filteredData = selectedYear ? rounds.filter(item => new Date(item.draw_at).getFullYear() === selectedYear) : rounds;
-  const filteredData = rounds.filter(
-    item => {
-      const yearMatch = !selectedYear || new Date(item.draw_at).getFullYear() === selectedYear;
-      const nameMatch = !selectedName || item.name === selectedName;
-      return yearMatch && nameMatch;
-    }
-  );
+
+  const filteredData = rounds.filter(item => {  
+    return !selectedYear || new Date(item.draw_at).getFullYear() === selectedYear 
+  }).filter(item => {  
+    return !selectedName || item.name.startsWith(selectedName);  
+  });
 
   //a function to display the message when there is no data
   const renderNoDataMessage = () => {
@@ -45,27 +44,27 @@ const App = () => {
   }
 
   // fetch the data after this component is mounted
-  // useEffect(() => {
-  //   fetchRounds();
-  // }, []);
+  useEffect(() => {
+    fetchRounds();
+  }, []);
 
   return (
       <div>
       <h3>Rounds Number: {rounds.length}</h3>
-      <button class="button is-success" onClick={fetchRounds}>Fetch</button>
+      {/*<button class="button is-success" onClick={fetchRounds}>Fetch</button>*/}
 
       {/* dropdown menu to select year */}
-      <select onChange={(event) => filterDataByYearAndName(parseInt(event.target.value), selectedName)}>
+      <select value={selectedYear} onChange={(event) => filterDataByYearAndName(parseInt(event.target.value), selectedName)}>
         <option value="">All Years</option>
         {/* Extract unique years from data */}
         {Array.from(new Set(rounds.map(item => new Date(item.draw_at).getFullYear()))).map(year => (<option key={year} value={year}>{year}</option>))}
       </select>
 
       {/* dropdown menu to select name */}
-      <select onChange={(event) => filterDataByYearAndName(selectedYear, event.target.value)}>
+      <select value={selectedName} onChange={(event) => filterDataByYearAndName(selectedYear, event.target.value)}>
         <option value="">All Names</option>
         {/* Extract unique names from data */}
-        {Array.from(new Set(rounds.map(item => item.name))).map(name => (
+        {Array.from(new Set(rounds.map(item => item.name.replace(/\([^)]*\)/g, '')))).map(name => (
           <option key={name} value={name}>{name}</option>
         ))}
       </select>
@@ -73,17 +72,18 @@ const App = () => {
       {renderNoDataMessage()}
 
       {/* show time and size in bar chart */}
-      <BarChart width={1000} height={300} data={filteredData}>
+      <ResponsiveContainer width="95%" height={500}>
+      <BarChart data={filteredData} margin={{ top: 50, right: 30, left: 50, bottom: 50 }}>
       <XAxis dataKey="draw_at" stroke="#8884d8" tickFormatter={(value) => new Date(value).toISOString().split('T')[0]} />{/*Format date to show only date part*/}
       <YAxis />
-      <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} labelFormatter={(value) => new Date(value).toISOString().split('T')[0]} 
+      <Tooltip wrapperStyle={{ width: "auto", backgroundColor: '#ccc' }} labelFormatter={(value) => new Date(value).toISOString().split('T')[0]} 
       formatter={(value, name, props) => [`Number of People: ${value}`]}/>
 
       {/* <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} /> */}
       <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
       <Bar dataKey="size" fill="#8884d8" barSize={25}/>
       </BarChart>
-
+      </ResponsiveContainer>
       </div>
       
   );
